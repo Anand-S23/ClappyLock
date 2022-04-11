08ce303bf58753e05627a8a1d1b348defb5f7e9a
Anand-S23 <anandsroopsing23@gmail.com>
2022-04-11 13:02:15 -0400

    Inital commit + Bluetooth setup

---
+++ app/src/main/java/com/example/clappylock/ControlActivity.kt
@@ -0,0 +1,110 @@
+package com.example.clappylock
+
+import android.Manifest
+import android.app.Activity
+import android.app.ProgressDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.os.AsyncTask
+import android.os.Bundle
+import java.util.*
+import android.bluetooth.BluetoothSocket
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.io.IOException
+
+
+
+class ControlActivity: AppCompatActivity() {
+
+    companion object {
+        var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        var m_bluetoothSocket: BluetoothSocket? = null
+        lateinit var m_progress: ProgressDialog
+        lateinit var m_bluetoothAdapter: BluetoothAdapter
+        var m_isConnected: Boolean = false
+        lateinit var m_address: String
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.control_layout)
+    }
+
+    private fun sendCommand(input: String) {
+        if (m_bluetoothSocket != null) {
+            try{
+                m_bluetoothSocket!!.outputStream.write(input.toByteArray())
+            } catch(e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun disconnect() {
+        if (m_bluetoothSocket != null) {
+            try {
+                m_bluetoothSocket!!.close()
+                m_bluetoothSocket = null
+                m_isConnected = false
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        finish()
+    }
+
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
+        private var connectSuccess: Boolean = true
+        private val context: Context = c
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            m_progress = ProgressDialog.show(context, "Connecting...", "please wait")
+        }
+
+        override fun doInBackground(vararg p0: Void?): String? {
+            try {
+                if (m_bluetoothSocket == null || !m_isConnected) {
+                    m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+                    val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
+                    if (ActivityCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return null
+                    }
+                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+                    m_bluetoothSocket!!.connect()
+                }
+            } catch (e: IOException) {
+                connectSuccess = false
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (!connectSuccess) {
+                Log.i("data", "couldn't connect")
+            } else {
+                m_isConnected = true
+            }
+            m_progress.dismiss()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/clappylock/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.clappylock
+
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import androidx.core.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private val ENABLE_BLUETOOTH_REQUEST = 1
+    private val ENABLE_DISCOVER_REQUEST = 2
+
+    companion object {
+        val EXTRA_ADDRESS: String = "extraAddress"
+    }
+
+    private lateinit var selectDeviceList: ListView
+    private lateinit var selectDeviceRefresh: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (bluetoothAdapter == null) {
+            val msg = "This device does not support bluetooth"
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            val permissionGranted = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED
+
+            if (!permissionGranted) {
+                val msg = "Bluetooth permissions required"
+                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+            }
+
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST)
+        }
+
+        if (!bluetoothAdapter.isDiscovering) {
+            val msg = "Making device discoverable"
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+
+            val enableDiscoverIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+            startActivityForResult(enableDiscoverIntent, ENABLE_DISCOVER_REQUEST)
+        }
+
+        selectDeviceList = findViewById(R.id.select_device_list)
+        selectDeviceRefresh = findViewById(R.id.select_device_refresh)
+
+        selectDeviceRefresh.setOnClickListener {
+            getDevicesPaired()
+        }
+    }
+
+    private fun getDevicesPaired() {
+        if (bluetoothAdapter.isEnabled) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                val msg = "Make device discoverable first"
+                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+            }
+
+            val devices = bluetoothAdapter.bondedDevices
+            val list: ArrayList<BluetoothDevice> = ArrayList()
+            if (devices.isNotEmpty()) {
+                for (device: BluetoothDevice in devices) {
+                    list.add(device)
+                    val msg = device.name
+                    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                val msg = "No paired device found"
+                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+            }
+
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+            selectDeviceList.adapter = adapter
+            selectDeviceList.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                val device: BluetoothDevice = list[position]
+                val address: String = device.address
+
+                val intent = Intent(this, ControlActivity::class.java)
+                intent.putExtra(EXTRA_ADDRESS, address)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            ENABLE_BLUETOOTH_REQUEST -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    val msg = "Connected to bluetooth"
+                    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                } else {
+                    val msg = "Could not connect to bluetooth"
+                    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                }
+            }
+
+            ENABLE_DISCOVER_REQUEST -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    val msg = "Device is discoverable"
+                    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                } else {
+                    val msg = "Could not make device discoverable"
+                    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
